65c36bae622ea7de447464271ab3eeca2bdd1964
carlosmot <carlos.motag@gmail.com>
2014-07-08 14:04:15 +0200

    Estado inicial - 0.0 SNAPSHOT

---
+++ ProyectoPrimeFaces4/src/main/java/com/pruebas/controller/MemberRegistration.java
@@ -0,0 +1,56 @@
+package com.pruebas.controller;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import com.pruebas.model.Member;
+
+// Modificaciones iniciales 123  
+// The @Stateful annotation eliminates the need for manual transaction demarcation
+@Stateful
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://sfwk.org/Documentation/WhatIsThePurposeOfTheModelAnnotation
+@Model  
+public class MemberRegistration {
+   private String s2;
+
+   @Inject
+   private Logger log;
+
+   @Inject
+   private EntityManager em;
+
+   @Inject
+   private Event<Member> memberEventSrc;
+
+   private Member newMember;
+
+   @Produces
+   @Named
+   public Member getNewMember() {
+      return newMember;
+   }
+
+   public String register() throws Exception {
+      log.info("Registering " + newMember.getName());
+      em.persist(newMember);
+      memberEventSrc.fire(newMember);
+      initNewMember();
+      return null;
+   }
+
+   @PostConstruct
+   public void initNewMember() {
+      newMember = new Member();
+   }
+}
